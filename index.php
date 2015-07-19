f4f08cf749aa56e4bc77b2935d36e1b8aed1bf72
kenyk7 <kenykalin@gmail.com>
2015-07-18 19:32:49 -0500

    update title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>YouTube youtube MDL</title>
+	<title>YouTube MDL</title>
 	<link rel="shortcut icon" href="favicon.png" />
 	<link rel="stylesheet" href="assets/css/material.red.css">
 	<script src="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.min.js"></script>
